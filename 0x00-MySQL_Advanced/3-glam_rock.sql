fb137f25ac64805a0ba4bec3d5f37bea83039da3
Martin Mwenda <mwirigi.8kabush@gmail.com>
2024-10-18 02:42:40 +0000

    buy buy

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,15 +2,6 @@
 -- The lifespan is calculated based on the 'formed' and 'split' columns.
 -- If the band has not split (NULL value in 'split'), we consider they are active until 2022.
 
-SELECT 
-    name AS band_name, 
-    CASE 
-        WHEN split IS NULL THEN 2022 - formed 
-        ELSE split - formed 
-    END AS lifespan
-FROM 
-    metal_bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
-    lifespan DESC;
+SELECT `band_name`, (IFNULL(`split`, '2022') - `formed`) AS `lifespan`
+    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', IFNULL(`style`, "")) > 0
+    ORDER BY `lifespan` DESC;
